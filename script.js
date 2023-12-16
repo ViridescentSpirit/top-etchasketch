5f260b51ac074b06472b516f1dc7aef0c6337c8d
Lane Bye <viridescentspiritgaming@gmail.com>
2023-12-16 17:32:45 -0600

    Finish main etch a sketch project
    Added and programmed a button to change the grid size to the users
    preference. Maximum size set at 100x100 to ensure that too many
    resources are not used to create the grid. Tested with multiple
    different sizes.

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 let gridSize = 16;
+const etchASketch = document.querySelector(`.etchASketch`);
 
 function createGrid(gridSize) {
-    const etchASketch = document.querySelector(`.etchASketch`);
-
     for (i = 0; i < gridSize; i++) {
         const newRow = document.createElement(`div`);
         newRow.classList = `gridRow`;
@@ -18,20 +17,49 @@ function createGrid(gridSize) {
 }
 
 createGrid(gridSize);
+activateSquares();
 
-const gridElements = document.querySelectorAll(`.gridCol`);
 
-gridElements.forEach(function(square) {
-    square.addEventListener(`mouseover`,function(){
-        this.style.backgroundColor = `blue`;
+function activateSquares() {
+    const gridElements = document.querySelectorAll(`.gridCol`);
+
+    gridElements.forEach(function(square) {
+        square.addEventListener(`mouseover`,function(){
+            this.style.backgroundColor = `blue`;
+        });
     });
-});
-
+}
 const resetButton = document.querySelector(`#resetButton`);
 resetButton.addEventListener(`click`, clearAllSquares);
 
 function clearAllSquares() {
+    const gridElements = document.querySelectorAll(`.gridCol`);
+
     gridElements.forEach(function(square) {
         square.style.backgroundColor = `white`;
     });
 };
+
+const sizeButton = document.querySelector(`#sizeButton`);
+sizeButton.addEventListener(`click`, promptSize);
+
+function promptSize() {
+    gridSize = prompt(`Please enter desired number of rows and columns.`);
+
+    if (gridSize > 100) {
+        gridSize = 100;
+        alert(`Maximum grid size is 100x100. Grid size set to 100x100!`);
+    }
+        
+    clearSquares();
+    createGrid(gridSize);
+    activateSquares();
+}
+
+function clearSquares() {
+    let child = etchASketch.lastElementChild;
+    while (child) {
+        etchASketch.removeChild(child);
+        child = etchASketch.lastElementChild;
+    }
+}
